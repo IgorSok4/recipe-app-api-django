85646ccec43d85237ab536f048e7dfa4eee0f23a
IgorS <sok.igor4@gmail.com>
2023-11-05 18:16:33 +0100

    implemented custom user model

--- app/app/settings.py
+++ app/app/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     #apps
     'core',
-    
+
     #django stuff
     'django.contrib.admin',
     'django.contrib.auth',
@@ -130,3 +130,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User'
